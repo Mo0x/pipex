9ed790ff99aed9c997fc1dd8cf86d2124dcae959
Max Govindaraju <mgovinda@k2r2p3.42mulhouse.fr>
2023-11-16 16:10:53 +0100

    some work

--- srcs/arg_parser.c
+++ srcs/arg_parser.c
@@ -1,33 +1,14 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   arg_parser.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/11/10 17:12:58 by mgovinda          #+#    #+#             */
-/*   Updated: 2023/11/16 15:36:42 by mgovinda         ###   ########.fr       */
+/*   Created: 2023/11/16 15:35:06 by mgovinda          #+#    #+#             */
+/*   Updated: 2023/11/16 15:57:38 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "../includes/pipex.h"
+#include "pipex.h"
 
-/*
-open file1 (create rdonly)
-open file1(create, write)
-fil1 cmd 1 | cmd2 >> file 2
-*/
-
-int	main(int argc, char **argv, char **envp)
-{
-	int	f1;
-	int	f2;
-
-	f1 = open(argv[1], O_RDONLY);
-	f2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if (f1 < 0 || f2 < 0)
-	// put erorr msg here
-		return(-1);
-	pipex(f1, f2, argv, envp);
-	return (0);
-}
--- srcs/pipex.c
+++ srcs/pipex.c
@@ -6,13 +6,39 @@
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/09 16:42:24 by mgovinda          #+#    #+#             */
-/*   Updated: 2023/11/09 16:45:07 by mgovinda         ###   ########.fr       */
+/*   Updated: 2023/11/16 16:08:47 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void foo(void)
+int process_one(int f1, char *cmd)
 {
-	write(1, "foo\n", 4);
+	char	**split;
+
+	split = ft_split(cmd, ' ');
+}
+
+void	pipex(int f1, int f2, char **argv, char **envp)
+{
+	int		super_pipe[2];
+	int		status;
+	pid_t	child_1;
+	pid_t	child_2;
+
+	pipe(super_pipe);
+	child_1 = fork();
+	if (child_1 < 0)
+		return (perror("Fork: "));
+	if (child_1 == 0)
+		process_one(f1, argv[2]);
+	child_2 = fork();
+	if (child_2 < 0)
+		return (perror("Fork: "));
+	if (child_2 == 0)
+		process_two(f2, argv[3]);
+	close(super_pipe[0]);
+	close(super_pipe[1]);
+	waitpid(child_1, &status, 0);
+	waitpid(child_2, &status, 0);
 }
