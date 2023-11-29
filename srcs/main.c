3839b561efc06b44fb27c49edcdb5d3e7844545f
Max Govindaraju <mgovinda@k2r4p3.42mulhouse.fr>
2023-11-29 15:41:17 +0100

    more

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: mgovinda <mgovinda@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/10 17:12:58 by mgovinda          #+#    #+#             */
-/*   Updated: 2023/11/29 14:56:03 by mgovinda         ###   ########.fr       */
+/*   Updated: 2023/11/29 15:34:45 by mgovinda         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,12 +25,24 @@ int	main(int argc, char **argv, char **envp)
 
 	if (argc != 4)
 		return (ft_argc_error(argv[0]));
-	f1 = open(argv[1], O_RDONLY);
-	f2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
-	if (f1 < 0 || f2 < 0)
-	// put erorr msg here
-		return(-1);
-	ft_printf(1, "%s", envp);
-	//pipex(f1, f2, argv, envp);
+	if (access(argv[1], F_OK | R_OK))
+		f1 = open(argv[1], O_RDONLY);
+	else
+		perror();
+	if (f1 < 0)
+	{
+		perror("Error opening file1 :");
+		return (-1);
+	}
+	if (access(argv[4], F_OK | W_OK | R_OK))
+		f2 = open(argv[4], O_CREAT | O_RDWR | O_TRUNC, 0644);
+	else
+		perror();
+	if (f2 < 0)
+	{
+		perror("Error opening file2 :");
+		return (-1);
+	}
+	pipex(f1, f2, argv, envp);
 	return (0);
 }
